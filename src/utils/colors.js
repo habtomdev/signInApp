bb210747c6a9e4b1739cead57aa27754cd7ef1bc
Habtom <habtomst221@gmail.com>
2024-07-19 22:12:04 +0300

    created color and font js file

---
+++ src/utils/colors.js
@@ -0,0 +1,6 @@
+export const colors = {
+    white: "#F7FBFC",
+    blueish: "#769FCD",
+    gray: "#D6E6F2",
+    lightBlue: "#B9D7EA",
+}
---
+++ src/utils/fonts.js
@@ -0,0 +1,8 @@
+export const fonts = {
+    Bold: "Poppins-Bold",
+    Italic: "Poppins-Italic",
+    Light: "Poppins-Light",
+    Medium: "Poppins-Bold",
+    Regular: "Poppins-Regular",
+    Thin: "Poppins-Thin"
+}
