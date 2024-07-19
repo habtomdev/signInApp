519c9d9d97df846452ba5f49a3718fefa6bfcb0f
Habtom <habtomst221@gmail.com>
2024-07-19 23:17:43 +0300

    Button styling
    Added buttons and styling to them.

--- src/screen/HomeScreen.jsx
+++ src/screen/HomeScreen.jsx
@@ -1,4 +1,4 @@
-import { Image, StyleSheet, Text, View } from 'react-native'
+import { Image, StyleSheet, Text, TouchableOpacity, View } from 'react-native'
 import React from 'react'
 import { colors } from '../utils/colors'
 import { fonts } from '../utils/fonts'
@@ -9,7 +9,17 @@ const HomeScreen = () => {
             <Image source={require("../assets/Logo.png")} style={styles.logo}/>
             {/* added logo */}
             <Image source={require("../assets/Group-img.png")} style={styles.group}/>
-            <Text style={styles.title}>WELCOME</Text>
+            <Text style={styles.title}>Hello,</Text>
+            <Text style={styles.subtitle}>Welcome!</Text>
+            <View style={styles.btnContainer}>
+                <TouchableOpacity style={styles.loginBtnWrapper}>
+                    <Text style={styles.loginBtnText}>Log-In</Text>
+                </TouchableOpacity>
+                <TouchableOpacity style={styles.signinBtnWrapper}>
+                    <Text style={styles.loginBtnText}>Sign-Up</Text>
+                </TouchableOpacity>
+            </View>
+                
         </View>
     )
 }
@@ -19,16 +29,16 @@ export default HomeScreen
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        backgroundColor: colors.white,
         alignItems: "center",
-        display: "flex",
-        flexDirection: "column",
-
+        margin: 30,
+        color: colors.cream,
     },
     
     logo: {
         height: 60,
         width: 180,
+        marginTop: 30,
+        marginBottom: 80,
     },
 
     group: {
@@ -40,6 +50,42 @@ const styles = StyleSheet.create({
     title: {
         fontSize: 40,
         fontFamily: fonts.Regular,
+        color: 'black',
     },
+
+    subtitle: {
+        fontFamily:fonts.Regular,
+        fontSize: 30,
+        color: "darkblue",
+    },
+
+    btnContainer: {
+        flexDirection: "row",
+        borderWidth: 2,
+        borderColor: "black",
+        width: "90%",
+        height: 60,
+        marginTop: 50,
+        borderRadius: 100,
+    },
+
+    loginBtnWrapper: {
+        alignItems: "center",
+        justifyContent: "center",
+        width: "50%",
+        backgroundColor: "black",
+        borderRadius: 100,
+    },
+
+    loginBtnText: {
+        fontFamily: fonts.Bold,
+        color: colors.cream,
+        fontSize: 20,
+    },
+
+    signinBtnWrapper: {
+
+    },
+
 })
 
--- src/utils/colors.js
+++ src/utils/colors.js
@@ -1,5 +1,5 @@
 export const colors = {
-    white: "#F7FBFC",
+    cream: "#F7FBFC",
     blueish: "#769FCD",
     gray: "#D6E6F2",
     lightBlue: "#B9D7EA",
